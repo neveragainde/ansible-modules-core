f83c6516b1b1b41691145c1b8694e28604b7fecc
neveragainde <github@neveragain.de>
2015-12-17 23:21:05 +0100

    do NOT run service_enable_rcconf() inside a try: block, as use in Check Mode may lead to exit_json() and thus sys.exit(), which raises a SystemExit exception, which then erroneously generates an error
    instead, wrap reading and writing of rc.conf in try: blocks,
    providing sensible errors messages for IOErrors
    
    Fixes: Issue #2664 "unable to set rcvar"

--- system/service.py
+++ system/service.py
@@ -326,30 +326,35 @@ class Service(object):
 
         self.changed = None
         entry = '%s="%s"\n' % (self.rcconf_key, self.rcconf_value)
-        RCFILE = open(self.rcconf_file, "r")
-        new_rc_conf = []
 
-        # Build a list containing the possibly modified file.
-        for rcline in RCFILE:
-            # Parse line removing whitespaces, quotes, etc.
-            rcarray = shlex.split(rcline, comments=True)
-            if len(rcarray) >= 1 and '=' in rcarray[0]:
-                (key, value) = rcarray[0].split("=", 1)
-                if key == self.rcconf_key:
-                    if value.upper() == self.rcconf_value:
-                        # Since the proper entry already exists we can stop iterating.
-                        self.changed = False
-                        break
-                    else:
-                        # We found the key but the value is wrong, replace with new entry.
-                        rcline = entry
-                        self.changed = True
+        try:
+            RCFILE = open(self.rcconf_file, "r")
+            new_rc_conf = []
 
-            # Add line to the list.
-            new_rc_conf.append(rcline)
+            # Build a list containing the possibly modified file.
+            for rcline in RCFILE:
+                # Parse line removing whitespaces, quotes, etc.
+                rcarray = shlex.split(rcline, comments=True)
+                if len(rcarray) >= 1 and '=' in rcarray[0]:
+                    (key, value) = rcarray[0].split("=", 1)
+                    if key == self.rcconf_key:
+                        if value.upper() == self.rcconf_value:
+                            # Since the proper entry already exists we can stop iterating.
+                            self.changed = False
+                            break
+                        else:
+                            # We found the key but the value is wrong, replace with new entry.
+                            rcline = entry
+                            self.changed = True
 
-        # We are done with reading the current rc.conf, close it.
-        RCFILE.close()
+                # Add line to the list.
+                new_rc_conf.append(rcline)
+
+            # We are done with reading the current rc.conf, close it.
+            RCFILE.close()
+
+        except IOError as e:
+            self.module.fail_json(msg="Error reading %s: %s" % (self.rcconf_file, e.strerror))
 
         # If we did not see any trace of our entry we need to add it.
         if self.changed is None:
@@ -361,21 +366,25 @@ class Service(object):
             if self.module.check_mode:
                 self.module.exit_json(changed=True, msg="changing service enablement")
 
-            # Create a temporary file next to the current rc.conf (so we stay on the same filesystem).
-            # This way the replacement operation is atomic.
-            rcconf_dir = os.path.dirname(self.rcconf_file)
-            rcconf_base = os.path.basename(self.rcconf_file)
-            (TMP_RCCONF, tmp_rcconf_file) = tempfile.mkstemp(dir=rcconf_dir, prefix="%s-" % rcconf_base)
+            try:
+                # Create a temporary file next to the current rc.conf (so we stay on the same filesystem).
+                # This way the replacement operation is atomic.
+                rcconf_dir = os.path.dirname(self.rcconf_file)
+                rcconf_base = os.path.basename(self.rcconf_file)
+                (TMP_RCCONF, tmp_rcconf_file) = tempfile.mkstemp(dir=rcconf_dir, prefix="%s-" % rcconf_base)
 
-            # Write out the contents of the list into our temporary file.
-            for rcline in new_rc_conf:
-                os.write(TMP_RCCONF, rcline)
+                # Write out the contents of the list into our temporary file.
+                for rcline in new_rc_conf:
+                    os.write(TMP_RCCONF, rcline)
 
-            # Close temporary file.
-            os.close(TMP_RCCONF)
+                # Close temporary file.
+                os.close(TMP_RCCONF)
 
-            # Replace previous rc.conf.
-            self.module.atomic_move(tmp_rcconf_file, self.rcconf_file)
+                # Replace previous rc.conf.
+                self.module.atomic_move(tmp_rcconf_file, self.rcconf_file)
+
+            except IOError as e:
+                self.module.fail_json(msg="Error replacing %s: %s" % (self.rcconf_file, e.strerror))
 
 # ===========================================
 # Subclass: Linux
@@ -960,10 +969,7 @@ class FreeBsdService(Service):
         if self.rcconf_key is None:
             self.module.fail_json(msg="unable to determine rcvar", stdout=stdout, stderr=stderr)
 
-        try:
-            return self.service_enable_rcconf()
-        except:
-            self.module.fail_json(msg='unable to set rcvar')
+        return self.service_enable_rcconf()
 
     def service_control(self):
 
